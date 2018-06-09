78828f46883ab834a8767a5ef4664bef9c0ec0f5
Ron Shaiko <Ron.Shaiko@amdocs.com>
2018-06-09 17:58:22 +0300

    changed logs directory

--- todo_app/utilities/log_utility.py
+++ todo_app/utilities/log_utility.py
@@ -5,7 +5,7 @@ import sys
 def setup_custom_logger(name):
     formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(filename)s:%(lineno)d [ %(message)s ]',
                                   datefmt='%Y-%m-%d %H:%M:%S')
-    handler = logging.FileHandler('logs/' + str(name) + '.txt', mode='w')
+    handler = logging.FileHandler('/tmp/' + str(name) + '.txt', mode='w')
     handler.setFormatter(formatter)
     screen_handler = logging.StreamHandler(stream=sys.stdout)
     screen_handler.setFormatter(formatter)
